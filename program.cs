67623a4ebc0f39e9ccb6032a949db980ec8911ef
Айзек <popancik7@gmail.com>
2026-04-21 19:16:19 +0300

    Добавлен основной код программы

--- program.cs
+++ program.cs
@@ -1,1 +1,13 @@
+using System;
 
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет, мир!");
+            Console.ReadKey(); // Чтобы консоль не закрывалась сразу
+        }
+    }
+}
